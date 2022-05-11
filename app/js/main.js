692805ac234ca2a2d958c445af6d8ba9cfb257b3
LioraProgram7 <sv.kiselis@gmail.com>
2022-05-11 14:04:24 +0300

    adaptive header

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,11 @@
 $(function () {
 
+    $('.menu__burger, .menu a').on('click', function () {
+      $('.menu__list, .menu__burger').toggleClass('active');
+    });
+
+
+    
   $('.product-tabs__top-item').on('click', function (e) {
     e.preventDefault();
     $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
